123229d2c3826b12713d382ff134d13bcde4780f
Jonpor13 <jonpor13@gmail.com>
2022-06-18 16:05:43 +0200

    nuevos botones

--- src/app/components/administrador/administrador.component.ts
+++ src/app/components/administrador/administrador.component.ts
@@ -8,20 +8,76 @@ import { Router } from '@angular/router';
 })
 export class AdministradorComponent implements OnInit {
 
-  hidden: boolean = true;
+  hiddenRegister: boolean = true;
+  hiddenSituation: boolean = true;
+  hiddenGroup: boolean = true;
+  hiddenGrade: boolean = true;
+  hiddenCourse: boolean = true;
+  hiddenEncuesta: boolean = true;
+  hiddenPregunta: boolean = true;
+  hiddenOpcPregunta: boolean = true;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-    if (this.hidden){
-      this.hidden = false;
+  onRegister(){
+    if (this.hiddenRegister){
+      this.hiddenRegister = false;
     } else{
-      this.hidden = true;
+      this.hiddenRegister = true;
     }
-    //this.router.navigate(['/'+tipo]);
+  }
+
+  onSituacion(){
+    if (this.hiddenSituation){
+      this.hiddenSituation = false;
+    } else{
+      this.hiddenSituation = true;
+    }
+  }
+  onGrupo(){
+    if (this.hiddenGroup){
+      this.hiddenGroup = false;
+    } else{
+      this.hiddenGroup = true;
+    }
+  }
+  onGrado(){
+    if (this.hiddenGrade){
+      this.hiddenGrade = false;
+    } else{
+      this.hiddenGrade = true;
+    }
+  }
+  onAsignatura(){
+    if (this.hiddenCourse){
+      this.hiddenCourse = false;
+    } else{
+      this.hiddenCourse = true;
+    }
+  }
+  onEncuesta(){
+    if (this.hiddenEncuesta){
+      this.hiddenEncuesta = false;
+    } else{
+      this.hiddenEncuesta = true;
+    }
+  }
+  onPregunta(){
+    if (this.hiddenPregunta){
+      this.hiddenPregunta = false;
+    } else{
+      this.hiddenPregunta = true;
+    }
+  }
+  onOpcPregunta(){
+    if (this.hiddenOpcPregunta){
+      this.hiddenOpcPregunta = false;
+    } else{
+      this.hiddenOpcPregunta = true;
+    }
   }
 
 }
